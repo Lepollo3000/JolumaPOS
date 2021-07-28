0f81aa9a85a84fa5532439d29a0acf722ee2e9da
jlepe <josemanuel.lepe@gmail.com>
2021-07-28 16:16:14 -0500

    Arregladas pantallas para lógica de usuarios

---
+++ JolumaPOS_v2/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using JolumaPOS_v2.Server.Data;
+using JolumaPOS_v2.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(JolumaPOS_v2.Server.Areas.Identity.IdentityHostingStartup))]
+namespace JolumaPOS_v2.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- JolumaPOS_v2/Server/Models/ApplicationUser.cs
+++ JolumaPOS_v2/Server/Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,13 @@ namespace JolumaPOS_v2.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public bool Lockout { get; set; }
+        [Required]
+        public bool ForgotPassword { get; set; }
     }
 }
